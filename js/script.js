6a0a4de83325a2400651f65f95108e3e302b7363
Ever <sasa777.7@gmail.com>
2026-07-30 11:40:07 -0600

    se agrego java script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Mensaje al cargar la página
+window.addEventListener("load", () => {
+    alert("¡Bienvenido a la Página de Ventas!");
+});
+
+// Cambiar el título al hacer clic
+document.addEventListener("click", () => {
+    document.title = "Página de Ventas";
+});
+
+// Mostrar la fecha y hora en la consola
+setInterval(() => {
+    console.clear();
+    console.log("Fecha y hora:", new Date().toLocaleString());
+}, 1000);
